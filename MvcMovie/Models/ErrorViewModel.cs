942e88990e5e184d9ecbe31fd9b7ef1d7be16d94
ADMIN <ADMIN@SUMIT-PC>
2018-03-30 13:35:45 +0530

    comment

--- MvcMovie/Models/ErrorViewModel.cs
+++ MvcMovie/Models/ErrorViewModel.cs
@@ -4,6 +4,9 @@ namespace MvcMovie.Models
 {
     public class ErrorViewModel
     {
+
+
+
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
